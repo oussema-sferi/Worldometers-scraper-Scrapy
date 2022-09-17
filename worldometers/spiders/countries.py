4ebc354cceb816681e1baf5c1b1c67a3ce824c15
Sam <sam.s@itstacks.net>
2022-09-17 01:41:10 +0100

    iterating through countries link and scraping its informations

--- worldometers/spiders/countries.py
+++ worldometers/spiders/countries.py
@@ -1,4 +1,5 @@
 import scrapy
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
@@ -7,10 +8,22 @@ class CountriesSpider(scrapy.Spider):
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        title = response.xpath("//h1/text()").get()
-        countries = response.xpath("//td/a/text()").getall()
+        countries = response.xpath("//td/a")
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
 
-        yield {
-            'title': title,
-            'countries': countries
-        }
+            # absolute_url = f"https://www.worldometers.info{link}"
+            # absolute_url = response.urljoin(link)
+            yield response.follow(url=link, callback=self.parse_country)
+
+    def parse_country(self, response):
+        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+            yield {
+                'year': year,
+                'population': population
+            }
+
